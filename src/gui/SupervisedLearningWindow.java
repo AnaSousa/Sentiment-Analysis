104bf73d6a2283c9a7082d76628ff922cb9b62d7
AnaSousa <ei11068@fe.up.pt>
2015-04-08 16:44:00 +0100

    supervised window

--- src/gui/SupervisedLearningWindow.java
+++ src/gui/SupervisedLearningWindow.java
@@ -14,7 +14,7 @@ import java.awt.Font;
 
 public class SupervisedLearningWindow {
 
-	private JFrame frmSupervisionLearning;
+	public JFrame windowLearning;
 
 	/**
 	 * Launch the application.
@@ -24,7 +24,7 @@ public class SupervisedLearningWindow {
 			public void run() {
 				try {
 					SupervisedLearningWindow window = new SupervisedLearningWindow();
-					window.frmSupervisionLearning.setVisible(true);
+					window.windowLearning.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -43,49 +43,49 @@ public class SupervisedLearningWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmSupervisionLearning = new JFrame();
-		frmSupervisionLearning.setResizable(false);
-		frmSupervisionLearning.setTitle("Supervised learning");
-		frmSupervisionLearning.setBounds(100, 100, 442, 220);
-		frmSupervisionLearning.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmSupervisionLearning.getContentPane().setLayout(null);
+		windowLearning = new JFrame();
+		windowLearning.setResizable(false);
+		windowLearning.setTitle("Supervised learning");
+		windowLearning.setBounds(100, 100, 442, 220);
+		windowLearning.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		windowLearning.getContentPane().setLayout(null);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		textArea.setBounds(12, 80, 412, 60);
-		textArea.setBorder(BorderFactory.createEtchedBorder());
-		frmSupervisionLearning.getContentPane().add(textArea);
+		JTextArea phrasesArea = new JTextArea();
+		phrasesArea.setEditable(false);
+		phrasesArea.setBounds(12, 80, 412, 60);
+		phrasesArea.setBorder(BorderFactory.createEtchedBorder());
+		windowLearning.getContentPane().add(phrasesArea);
 		
-		JButton btnPrevi = new JButton("Previous");
-		btnPrevi.setBounds(12, 150, 117, 25);
-		frmSupervisionLearning.getContentPane().add(btnPrevi);
+		JButton btnPrevious = new JButton("Previous");
+		btnPrevious.setBounds(12, 150, 117, 25);
+		windowLearning.getContentPane().add(btnPrevious);
 		
 		JButton btnNext = new JButton("Next");
 		btnNext.setBounds(307, 150, 117, 25);
-		frmSupervisionLearning.getContentPane().add(btnNext);
+		windowLearning.getContentPane().add(btnNext);
 		
 		ButtonGroup rdbtnsPolarity = new ButtonGroup();
 		
 		JRadioButton rdbtnPositive = new JRadioButton("Positive");
 		rdbtnPositive.setForeground(Color.GREEN);
 		rdbtnPositive.setBounds(311, 49, 149, 23);
-		frmSupervisionLearning.getContentPane().add(rdbtnPositive);
+		windowLearning.getContentPane().add(rdbtnPositive);
 		rdbtnsPolarity.add(rdbtnPositive);
 		
 		JRadioButton rdbtnNeutral = new JRadioButton("Neutral");
-		rdbtnNeutral.setBounds(169, 49, 149, 23);
-		frmSupervisionLearning.getContentPane().add(rdbtnNeutral);
+		rdbtnNeutral.setBounds(169, 49, 140, 24);
+		windowLearning.getContentPane().add(rdbtnNeutral);
 		rdbtnsPolarity.add(rdbtnNeutral);
 		
 		JRadioButton rdbtnNegative = new JRadioButton("Negative");
 		rdbtnNegative.setForeground(Color.RED);
-		rdbtnNegative.setBounds(30, 49, 149, 23);
-		frmSupervisionLearning.getContentPane().add(rdbtnNegative);
+		rdbtnNegative.setBounds(30, 49, 136, 25);
+		windowLearning.getContentPane().add(rdbtnNegative);
 		rdbtnsPolarity.add(rdbtnNegative);
 		
-		JLabel label = new JLabel("15/30");
-		label.setFont(new Font("Dialog", Font.BOLD, 16));
-		label.setBounds(195, 12, 64, 25);
-		frmSupervisionLearning.getContentPane().add(label);
+		JLabel labelTotal = new JLabel("15/30");
+		labelTotal.setFont(new Font("Dialog", Font.BOLD, 16));
+		labelTotal.setBounds(195, 12, 64, 25);
+		windowLearning.getContentPane().add(labelTotal);
 	}
 }
